9bb5f5e586ddabd56d82f9bb14c6e2999c087a94
Trayehunter1 <hunterjohnsoninfo@gmail.com>
2021-09-07 17:13:09 -0500

    set up models

---
+++ models/Quote.js
@@ -0,0 +1,67 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+const QuoteSchema = new Schema({
+    
+    quoteText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+QuoteSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Quote = model('Quote', QuoteSchema);
+
+module.exports = Quote
